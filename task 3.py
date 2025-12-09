b2ca26d05859f8c6c6c2a9fae13db195e4ac330c
Laimonas Mikalauskas <laimislietuvis@gmail.com>
2025-12-09 19:25:28 +0200

    Add files via upload

--- task 3.py
+++ task 3.py
@@ -1,14 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
-from sqlalchemy.sql.ddl import CreateTable
-
-# Configuration
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projektai.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
 db = SQLAlchemy(app)
 
@@ -17,9 +12,7 @@ class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     position = db.Column(db.String(100), nullable=False)
-
-    # Relation with FinishedWork (one project has many FinishedWork
-    to_finish_the_work = db.relationship('Finished Work', backref='project', cascade='all, delete-orphan')
+    finished_works = db.relationship('FinishedWork', backref='employee', cascade='all, delete-orphan')
 
 class FinishedWork(db.Model):
     __tablename__ = 'finished_work'
@@ -27,7 +20,4 @@ class FinishedWork(db.Model):
     work = db.Column(db.String(100), nullable=False)
     estimate = db.Column(db.Float, nullable=False)
     company = db.Column(db.String(100), nullable=False)
-
-
-
-
+    employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'))
