02eca4d712f4d1a1d96b4c711f804a0105cb1449
Laimonas Mikalauskas <laimislietuvis@gmail.com>
2026-02-14 19:57:03 +0200

    Update task 3.py

--- task 3.py
+++ task 3.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projektai.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projects.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
 db = SQLAlchemy(app)
